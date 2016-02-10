5cf4c9832864c759ff817a958b4a9d42ee29ef90
Petar Upinov <petar.upinov@gmail.com>
2016-02-11 00:41:42 +0200

    12. Edit on date 18.11.2015 - update utility lib version

--- _Soft_Library_Pesho_/utility.h
+++ _Soft_Library_Pesho_/utility.h
@@ -1,7 +1,7 @@
 /*************************************************************************
 *** LIBRARY: UTILITY                                         *************
 *** AUTHOR:  PETAR UPINOV, email: petar.upinov@gmail.com     *************
-*** FILE NAME: utility.h, v0.01, 18.10.2015                  *************
+*** FILE NAME: utility.h, v0.02, 18.11.2015                  *************
 *** SOFT IDE: AVR-GCC compiler                               *************
 *** HARD uCU: ATmel AVR Microcontrollers                     *************
 *** TEST: ATmega8535@16MHz, ATmega32@16MHz                   *************
